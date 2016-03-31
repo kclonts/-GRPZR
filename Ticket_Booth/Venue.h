745bfbd6e8837300b7c89260cbbf2b84cec43bfc
dakotahm <dakotah.moore523@gmail.com>
2016-03-31 15:33:21 -0400

    DisplayAll Update
    Added section and performance arrays as well as  a declaration for DisplayAll function;

--- Ticket_Booth/Venue.h
+++ Ticket_Booth/Venue.h
@@ -7,11 +7,15 @@ class Venue
 {
 public:
     static const int MAX_SEAT_ROWS = 1000;
+    static const int MAX_SECTIONS=100;
+    static const int MAX_PERFORM=100;
 
 private:
     string name;
     const Address* address;
     const Seat_Row* seat_rows[MAX_SEAT_ROWS];
+    const Section* sectionList[MAX_SECTIONS]; //These two may need to be changed depending on section class by TC
+    const Performance* performanceList[MAX_PERFORMANCES];
     int number_of_seat_rows;
 
 public:
